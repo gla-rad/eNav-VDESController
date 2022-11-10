d36eceaf27ade972787b2148477753ac5fb9efc5
Nikolaos Vastardis <n.vastardis@gmail.com>
2022-11-10 16:56:21 +0000

    Fixing the config classes

--- src/main/java/org/grad/eNav/vdesCtrl/config/GlobalConfig.java
+++ src/main/java/org/grad/eNav/vdesCtrl/config/GlobalConfig.java
@@ -1,7 +1,5 @@
 package org.grad.eNav.vdesCtrl.config;
 
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -9,12 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * The Boostrap Configuration.
+ * The Global Configuration.
+ *
+ * A class to define the global configuration for the application.
  *
  * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
  */
 @Configuration
-public class BootstrapConfig {
+public class GlobalConfig {
 
     /**
      * <p>
@@ -33,28 +33,6 @@ public class BootstrapConfig {
     @Bean
     public HttpTraceRepository httpTraceRepository() {
         return new InMemoryHttpTraceRepository();
-    }
-
-    /**
-     * Load Keycloak configuration from application.properties or application.yml
-     *
-     * On multi-tenant scenarios, Keycloak will defer the resolution of a
-     * KeycloakDeployment to the target application at the request-phase.
-     *
-     * A Request object is passed to the resolver and callers expect a complete
-     * KeycloakDeployment. Based on this KeycloakDeployment, Keycloak will
-     * resume authenticating and authorizing the request.
-     *
-     * This is required in a separate configuration according to:
-     * https://stackoverflow.com/questions/57957006/unable-to-build-spring-based-project-for-authentication-using-keycloak
-     *
-     * Otherwise, a circular dependency issues appear during startup.
-     *
-     * @return The keycloak configuration resolver
-     */
-    @Bean
-    public KeycloakConfigResolver keycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
     }
 
 }
