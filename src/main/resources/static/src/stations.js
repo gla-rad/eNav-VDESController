e68a0c8f67d0daa10b497d5203d925290c00cf3f
Nikolaos Vastardis <n.vastardis@gmail.com>
2023-08-15 09:23:05 +0100

    Adding the new datatables cosmetic changes

--- src/main/resources/static/src/stations.js
+++ src/main/resources/static/src/stations.js
@@ -119,9 +119,6 @@ var messageColumnDefs = [{
 $(function () {
     stationsTable = $('#stations_table').DataTable({
         processing: true,
-        language: {
-            processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span>',
-        },
         serverSide: true,
         ajax: {
             type: "POST",
